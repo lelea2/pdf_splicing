98f8997c0d063acadd9c2c360e87fd0604e630bd
lelea2 <daomaikhanh1988@yahoo.com>
2017-01-25 15:01:29 -0800

    Initial check in

--- pdf_scissors/index.js
+++ pdf_scissors/index.js
@@ -3,14 +3,14 @@
 var scissors = require('scissors');
 var fs = require('fs');
 
-// var pdf = scissors(__dirname + '/../mock/input/Breaks_GS-Unbalanced-qQs.pdf');
-var pdf = scissors(__dirname + '/../mock/input/test.pdf');
+var pdf = scissors(__dirname + '/../mock/input/Breaks_GS-Unbalanced-qQs.pdf');
+// var pdf = scissors(__dirname + '/../mock/input/test.pdf');
 var page = pdf.pages(2);
 
 // console.log(pages);
 
-// pages.pdfStream().pipe(fs.createWriteStream(__dirname + '/../mock/output/scissor_test.pdf'));
-var stream = page.pdfStream().pipe(fs.createWriteStream(__dirname + '/../mock/output/test_page.pdf'));
+var stream = page.pdfStream().pipe(fs.createWriteStream(__dirname + '/../mock/output/scissor_test.pdf'));
+// var stream = page.pdfStream().pipe(fs.createWriteStream(__dirname + '/../mock/output/test_page.pdf'));
 
 // All content
 pdf.contentStream().on('data', function (item) {
